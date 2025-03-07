bbe130d73d1aa07c9aa57562a5ea4e5afdb45559
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2025-03-07 19:09:31 +0530

    feat: :sparkles: add read file

---
+++ utils/read-file.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"io"
+	"os"
+)
+
+func ReadFileBytes(filePath string) ([]byte, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
+}
